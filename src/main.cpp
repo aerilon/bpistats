ca0be3086f9cd227c696ace6e07feb89cefd79a4
Arnaud Lacombe <lacombar@gmail.com>
2018-02-08 19:37:06 -0800

    Import pre-bonus statistics math details

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#include <limits.h>
+#include <cmath>
 #include <map>
 #include <string>
 #include <iostream>
@@ -178,7 +180,96 @@ main::operator()()
 
 	for (auto& records : this->file_records)
 	{
-		//io_service.post()
+
+		auto worker =
+		[&]()
+		{
+			auto& map = records.get();
+
+			auto size = map.size();
+
+			int i = 1;
+			int median_index = size / 2;
+			if (size % 2)
+			{
+				median_index += 1;
+			}
+			median_index += 1;
+
+			boost::posix_time::ptime lowest_pt, highest_pt;
+			long double lowest, highest, last_x, sum_x, sum_x2, median;
+
+			lowest = LDBL_MAX;
+			highest = 0;
+			last_x = 0;
+			sum_x = 0;
+			sum_x2 = 0;
+
+			// Welford's method, requires only 1 pass
+			for (const auto& row : map)
+			{
+				long double x = row.second;
+
+				sum_x += x;
+
+				sum_x2 += (x * x);
+
+				// Highest price
+				if (x > highest)
+				{
+					highest = x;
+					highest_pt = row.first;
+				}
+
+				if (x < lowest)
+				{
+					lowest = x;
+					lowest_pt = row.first;
+				}
+
+				if (i > median_index)
+				{
+					continue;
+				}
+
+				if (i == median_index)
+				{
+					median = last_x;
+					if ((static_cast<int>(size) % 2) == 0)
+					{
+						median += x;
+						median /= 2.0;
+					}
+				}
+
+				last_x = x;
+
+				i++;
+			}
+
+			auto mean = sum_x / size;
+			auto variance = (sum_x2 - (sum_x * sum_x) / size) / (size - 1);
+			auto stddev = std::sqrt(variance);
+
+			boost::property_tree::ptree statistics;
+
+			statistics.put("lowest.price", lowest);
+			statistics.put("lowest.date", lowest_pt);
+			statistics.put("highest.price", highest);
+			statistics.put("highest.date", highest_pt);
+			statistics.put("stddev", stddev);
+			statistics.put("average", mean);
+			statistics.put("median", median);
+
+			auto results = records.describe();
+
+			results.put_child("statistics", statistics);
+
+			this->print(results);
+
+		};
+
+		io_service.post(worker);
 	}
 
 	if (this->vm.count("range"))
